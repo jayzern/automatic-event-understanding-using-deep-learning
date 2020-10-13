fd21ba4f902ea27b50a3221e493780d2d20b61ee
kevin_wibisono <kw2870@columbia.edu>
2020-10-13 21:27:19 +0000

    Add code for wide-and-deep

--- src/event-rep/event-embedding/model/__init__.py
+++ src/event-rep/event-embedding/model/__init__.py
@@ -21,6 +21,9 @@ from .resrofa import MTRFv4Res
 # classname of ResRoFDense-MT: MTRFv4ResDense
 from .rofdense import MTRFv4ResDense
 
+# classname of RofAWD: MTRFv4WD
+from .rofawd_lwz import MTRFv4WD
+
 # ablation study: sigle task version for composition methods
 
 # classname of RoFA-ST: NNRF_ROFA
