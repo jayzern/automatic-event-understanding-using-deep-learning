96aeb2abe33cd55f1de448a0ddc5d16da398570b
jayzern <jayzern@hotmail.com>
2020-11-05 16:23:29 +0000

    add rofbeg_v2 and rofdense_v2

--- src/event-rep/event-embedding/config.py
+++ src/event-rep/event-embedding/config.py
@@ -35,8 +35,8 @@ DATA_PATH = os.path.join(BASE_DIR, 'data/')
 DATA_PATH = '/media/data-disk/data/event-rep/exp9_0.1-16-16-Roles2Args3Mods-NoFrAn-v1/'
 
 # data2 path
-DATA2_DIR = os.path.join(BASE_DIR, 'data/')
-# DATA2_DIR = '/media/data-disk/data/event-rep'
+# DATA2_DIR = os.path.join(BASE_DIR, 'data/')
+DATA2_DIR = '/media/data-disk/data/event-rep/'
 # DATA2_DIR = '/home/yn2373/seq_event-rep/'
 
 # Model path
--- src/event-rep/event-embedding/model/__init__.py
+++ src/event-rep/event-embedding/model/__init__.py
@@ -21,11 +21,17 @@ from .resrofa import MTRFv4Res
 # (TEAM2-change) classname of ResRoFDense-MT: MTRFv4ResDense
 from .rofdense import MTRFv4ResDense
 
+# (TEAM2-change) classname of ResRoFDense-MT: MTRFv4ResDense
+from .rofdense_v2 import MTRFv4ResDense_v2
+
 # (TEAM2-change) classname of RoFAWD-MT: MTRFv4WD
 from .rofawd import MTRFv4WD
 
 # (TEAM2-change) classname of RoFBeg-MT: MTRFv4RofBeg
 from .rofbeg import MTRFv4RofBeg
+
+# (TEAM2-change) classname of RoFBeg-MT: MTRFv4RofBeg
+from .rofbeg_v2 import MTRFv4RofBeg_v2
 
 # ablation study: sigle task version for composition methods
 
